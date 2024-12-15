fd40c802706ac74667df5c284ce71a88aa9e84b9
James Talamo <jamestalamo4@gmail.com>
2024-12-15 14:38:48 +0800

    1.3.9

--- src/pages/page6/Page6.jsx
+++ src/pages/page6/Page6.jsx
@@ -17,20 +17,20 @@ const Page6 = () => {
                 <img
                     src={`assets/page6/page6-img${select}.jpg`}
                 />
-                <div className='w-full h-[100px] flex items-center justify-center  bottom-[0%] bg-text-gray-custom '>
-                    <div className={`w-[100px] h-[100px] cursor-pointer ${select === 1 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(1)}>
+                <div className='w-full h-[100px] flex items-center justify-end  bottom-[0%] bg-text-gray-custom '>
+                    <div className={`w-[100px] h-[60px] cursor-pointer ${select === 1 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(1)}>
                         <img
                             src='assets/page6/page6-sel1.jpg'
                             className='w-full h-full'
                         />
                     </div>
-                    <div className={`w-[100px] h-[100px] cursor-pointer ${select === 3 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(3)}>
+                    <div className={`w-[100px] h-[60px] cursor-pointer ${select === 3 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(3)}>
                         <img
                             src='assets/page6/page6-sel2.jpg'
                             className='w-full h-full'
                         />
                     </div>
-                    <div className={`w-[100px] h-[100px] cursor-pointer ${select === 2 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(2)}>
+                    <div className={`w-[100px] h-[60px] cursor-pointer ${select === 2 ? 'border-2 border-solid border-black' : ''}`} onClick={() => changeSelect(2)}>
                         <img
                             src='assets/page6/page6-sel3.jpg'
                             className='w-full h-full'
